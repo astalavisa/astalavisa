b029f5df8c1a029721bd7362057fda8f93d046bd
FunctorFlow <functorflow@pm.me>
2019-09-03 12:49:18 +0000

    Update source/index.html.jsx

--- source/index.html.jsx
+++ source/index.html.jsx
@@ -1,24 +1,21 @@
 import Layout from './layout.html.jsx';
 import Header from './components/header.jsx';
+import Category from './components/category.jsx';
+import Countries from '../components/countries.jsx';
+import Employees from '../components/employees.jsx';
+import Plans from '../components/plans.jsx';
+// import ContactMap
+import Footer from '../components/footer.jsx';
 
 export default () => {
   return (
     <Layout>
       <Header/>
-
-      { /* Category Boxes || visa type */ }
-
-      { /* Category Boxes / End */ }
-
-
-      { /* Features Jobs */ }
-      { /* Featured Jobs / End */ }
-
-
-      { /* Features Cities */ }
-
-      { /* Features Cities / End */ }
-
+      <Category/>
+      <Countries/>
+      <Employees/>
+      <Plans/>
+      <Footer/>
     </Layout>
   );
 };
