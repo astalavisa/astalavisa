388b353938d587761d085551f89d56210d167652
FunctorFlow <functorflow@pm.me>
2019-09-03 14:25:28 +0000

    Update source/components/banner.html.jsx, source/components/category.html.jsx, source/components/countries.html.jsx, source/components/employees.html.jsx, source/components/footer.html.jsx, source/components/form.html.jsx, source/components/header.html.jsx, source/components/plans.html.jsx, source/index.html.jsx files

--- source/index.html.jsx
+++ source/index.html.jsx
@@ -1,10 +1,10 @@
 import Layout from './layout.html.jsx';
-import Header from './components/header.jsx';
-import Category from './components/category.jsx';
-import Countries from './components/countries.jsx';
-import Employees from './components/employees.jsx';
-import Plans from './components/plans.jsx';
-import Footer from './components/footer.jsx';
+import Header from './components/header.html.jsx';
+import Category from './components/category.html.jsx';
+import Countries from './components/countries.html.jsx';
+import Employees from './components/employees.html.jsx';
+import Plans from './components/plans.html.jsx';
+import Footer from './components/footer.html.jsx';
 
 export default () => {
   return (
