d7b141c5a8e3cdd2bde911ee7f335a767a1cd123
FunctorFlow <functorflow@pm.me>
2019-09-03 11:50:46 +0000

    Update source/layout.html.jsx, source/index.html.jsx files

--- source/index.html.jsx
+++ source/index.html.jsx
@@ -1,10 +1,28 @@
 import About from './pages/about.html.mdx';
 import Header from './components/header.jsx';
+import Banner from './components/banner.jsx';
 
 export default () => {
   return (
     <>
       <Header/>
+      <Banner/>
+
+      { /* Content
+      ================================================== */ }
+      { /* Category Boxes || visa type */ }
+
+      { /* Category Boxes / End */ }
+
+
+      { /* Features Jobs */ }
+      { /* Featured Jobs / End */ }
+
+
+      { /* Features Cities */ }
+
+      { /* Features Cities / End */ }
+
       <h1>Welcome to my new Charge site!</h1>
     </>
   );
--- source/layout.html.jsx
+++ source/layout.html.jsx
@@ -3,8 +3,21 @@
 
 export default function Layout({ children }) {
   return (
-    <div id="wrapper">
-      { children }
-    </div>
+    <>
+      <html lang="en">
+      <head>
+      <title>VISA with professional Registered Migration Agents</title>
+      <meta charset="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+      </head>
+      <body>
+
+      <div id="wrapper">
+        { children }
+      
+      </div>
+      </body>
+      </html>
+    </>
   );
 }
