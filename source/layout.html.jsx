e075a39f470cd53b3908b04f2fa9eecb46fbed44
FunctorFlow <functorflow@pm.me>
2019-09-03 13:04:33 +0000

    Update source/layout.html.jsx

--- source/layout.html.jsx
+++ source/layout.html.jsx
@@ -3,10 +3,11 @@
 
 export default function Layout({ children }) {
   return (
-    <>
+    <html>
       <head>
         <title>VISA with professional Registered Migration Agents</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
         <link rel="stylesheet" type="text/css" href="css/style.css"/>
         <link rel="stylesheet" type="text/css" href="css/colors/blue.css"/>
       </head>
@@ -17,6 +18,6 @@ export default function Layout({ children }) {
 
         
       </body>
-    </>
+    </html>
   );
 }
