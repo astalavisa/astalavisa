8532787989d2c00bf2f7426b4cc3a2c94bfb0478
astalavisa <54945137+astalavisa@users.noreply.github.com>
2019-09-16 20:48:47 +0500

    Update layout.html.jsx

--- source/layout.html.jsx
+++ source/layout.html.jsx
@@ -7,7 +7,6 @@ export default function Layout({ children }) {
         <link rel="stylesheet" type="text/css" href="css/style.css"/>
         <link rel="stylesheet" type="text/css" href="css/colors/blue.css"/>
         
-<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136068921-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
